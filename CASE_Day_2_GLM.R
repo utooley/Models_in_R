00bb6b4364ac9bfe448afa61a6a352fced394d1d
Ursula Tooley <utooley@gmail.com>
2016-05-18 17:06:19 -0700

    Initial day 2

---
+++ CASE_Day_2_GLM.R
@@ -0,0 +1,6 @@
+##Linear models are only appropriate when your outcome variable is continuous and unbounded
+
+#If you want to model a categorical outcome, or a bounded outcome (like a proportion or percent),
+#then you usually can't rely on regular linear modeling to do it
+##CAVEAT: sometimes you can, if you're not running into the edges of your boundaries (e.g. 0 or 100)
+
