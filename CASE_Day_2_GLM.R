319ae5baa2ae802e0568e0f7bea50e1241e22fc6
Ursula Tooley <utooley@gmail.com>
2016-05-18 17:12:53 -0700

    Changed name of testdoc to Day_1_testdoc

--- CASE_Day_2_GLM.R
+++ CASE_Day_2_GLM.R
@@ -4,3 +4,17 @@
 #then you usually can't rely on regular linear modeling to do it
 ##CAVEAT: sometimes you can, if you're not running into the edges of your boundaries (e.g. 0 or 100)
 
+##Tranform your outcome and input data: if it's binary (or bounded at 0 and 1), a logit works well
+#If count data, then logit works, etc.
+#These are "link functions" , and your outcomes will also be in that form 
+
+#No single analytic solution to GLMs, so comp uses brute force to find a solution, 
+#iterating over combos of parameter estimates and seeing which gives best model fit. 
+#So your results are not exact, and you may see small changes if you run the model again 
+#or with a different package. Or, your model may not converge!
+
+##If your model doesn't converge, don't use the output!
+
+library(ggplot2)
+library(knitr)
+library(car)
